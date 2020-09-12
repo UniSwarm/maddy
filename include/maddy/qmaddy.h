b5aeaa6c329067b131830f4e18b74946e3a17a1c
sebcaux <sebastien.caux@uniswarm.eu>
2020-09-12 17:43:19 +0200

    [qmaddy] Added an head for html with optional title and css

--- include/maddy/qmaddy.h
+++ include/maddy/qmaddy.h
@@ -4,6 +4,7 @@
 #include "maddy/parser.h"
 
 #include <QByteArray>
+#include <QString>
 
 namespace maddy {
 
@@ -25,14 +26,56 @@ public:
 
     QByteArray toHtml(QByteArray markdownSrc) const
     {
+        QByteArray out;
         std::stringstream markdownInput(markdownSrc.toStdString());
         std::string htmlOutput = parser->Parse(markdownInput);
-        return QByteArray::fromStdString(htmlOutput);
+        out.append("<!DOCTYPE html>");
+        out.append("<html lang=\"en\">");
+        out.append("<head>");
+        out.append("<meta charset=\"utf-8\">");
+        if (!_title.isEmpty())
+        {
+            out.append("<title>" + _title + "</title>");
+        }
+        if (!_css.isEmpty())
+        {
+            out.append("<link rel=\"stylesheet\" href=\"" + _css + "\">");
+        }
+        out.append("</head>");
+        out.append("<body>");
+        out.append("<div class=\"markdown-body\">\n");
+        out.append(QByteArray::fromStdString(htmlOutput));
+        out.append("</div>");
+        out.append("</body>");
+        out.append("</html>");
+        return out;
+    }
+
+    QString title() const
+    {
+        return _title;
+    }
+
+    void setTitle(const QString &title)
+    {
+        _title = title;
+    }
+
+    QString css() const
+    {
+        return _css;
+    }
+
+    void setCss(const QString &css)
+    {
+        _css = css;
     }
 
 private:
     std::shared_ptr<maddy::ParserConfig> config;
     std::shared_ptr<maddy::Parser> parser;
+    QString _title;
+    QString _css;
 };
 }
 #endif // QMADDY_H
